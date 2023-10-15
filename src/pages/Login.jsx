5376145536d45c3cc0c99b5ca8fac592d0f27101
danielwalker92 <danielrywalker@gmail.com>
2023-10-14 17:10:36 -0700

    Login page setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,11 +4,14 @@ import './Login.css'
 const Login = () => {
     const [ email, setEmail ] = useState('') 
     const [ password, setPassword ] = useState('') 
-    const [ modal, setModal ] = useState(true)
+    const [ modal, setModal ] = useState(false)
+    const [ loggedIn, setLoggedIn ] = useState(false)
+
+    const SERVER = `${import.meta.env.VITE_SERVER_HN}${import.meta.env.VITE_SERVER_PORT}`
 
     const loginUser = async (e) => {
         e.preventDefault()
-        const response = await fetch(`${import.meta.env.VITE_SERVER_HN}${import.meta.env.VITE_SERVER_PORT}/login`, {
+        const response = await fetch(`${SERVER}/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -23,17 +26,62 @@ const Login = () => {
         console.log(data)
     }
 
-    const registerUser = (e) => {
-        toggleModal()
+    const registerUser = async (e) => {
+       const response = await fetch(`${SERVER}/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+       })
+       const data = await response.json()
+       console.log(data)
+       toggleModal(e)
     }
 
-    const toggleModal = () => {
+    const toggleModal = (e) => {
+        e.preventDefault()
         setModal(!modal)
         setPassword('')
         setEmail('')
     }
  
     return  modal ? (
+        <main>
+                <div className="app-intro">
+                    <h1>Register</h1>
+                </div>
+                <form className="app-login"> 
+                    <input 
+                        type="text" 
+                        placeholder="Email" 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}    
+                    />
+                    <input
+                        type="password" 
+                        placeholder="Password" 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}    
+                    />
+                    <section>
+                    <input 
+                        type="button"
+                        value="Go Back"
+                        onClick={toggleModal}
+                    />
+                    <input 
+                        type="button"
+                        value="Register"
+                        onClick={registerUser}
+                    />
+                    </section>
+                </form>
+            </main>
+    ) : (
             <main>
                 <div className="app-intro">
                     <h1>Viedash</h1>
@@ -62,38 +110,6 @@ const Login = () => {
                         value="Register User"
                         className="register-btn"
                         onClick={toggleModal}
-                    />
-                    </section>
-                </form>
-            </main>
-    ) : (
-        <main>
-                <div className="app-intro">
-                    <h1>Register</h1>
-                </div>
-                <form className="app-login"> 
-                    <input 
-                        type="text" 
-                        placeholder="Email" 
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}    
-                    />
-                    <input
-                        type="password" 
-                        placeholder="Password" 
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}    
-                    />
-                    <section>
-                    <input 
-                        type="button"
-                        value="Go Back"
-                        onClick={toggleModal}
-                    />
-                    <input 
-                        type="button"
-                        value="Register"
-                        onClick={registerUser}
                     />
                     </section>
                 </form>
